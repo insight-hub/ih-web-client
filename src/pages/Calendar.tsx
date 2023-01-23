e69fad79b6d7d2f464607e8e27420ebf7fa7445f
saymurrmeow <r.savinovskii@gmail.com>
2023-01-23 04:08:17 +0300

    feat: calendar page

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ButtonGroup } from '../components/ui/buttonGroup/ButtonGroup';
 import { Button } from '../components/ui/button/Button';
@@ -23,16 +23,18 @@ export const Calendar = () => {
   const monthNum = timestamp.getMonth();
   const year = timestamp.getFullYear();
 
+  const [activeIdx, setActiveIdx] = useState(0);
+
   return (
     <>
       <div className="calentar_header col-lg-12">
         <div className="row align-center font-xl">
-          <div className="col-5-xs">
+          <div className="col-5-xs col-3-md">
             <span className="fw-bold">{months[monthNum]}</span> {year}
           </div>
           <div className="col-7-xs">
-            <ButtonGroup>
-              <Button title="Day" />
+            <ButtonGroup activeIdx={activeIdx} setActiveIdx={setActiveIdx}>
+              <Button title="Day" variant="primary" />
               <Button title="Week" />
               <Button title="Month" />
               <Button title="Year" />
