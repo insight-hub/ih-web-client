7045be1f16d3946dabdc76762c8f1e1da45a5f12
saymurrmeow <r.savinovskii@gmail.com>
2023-01-23 04:07:50 +0300

    feat(ui): button group

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,15 +1,21 @@
+import classNames from 'classnames';
 import React from 'react';
 
 import './Button.scss';
 
+type ButtonVariant = 'primary' | 'danger' | 'secondary';
+
 type Props = {
   title: string;
+  variant?: ButtonVariant;
 };
 
 export const Button: React.FC<Props> = (props) => {
+  const buttonClasses = classNames(['btn', `btn--${props.variant ?? 'primary'}`]);
+
   return (
     <>
-      <button className="btn">{props.title}</button>
+      <button className={buttonClasses}>{props.title}</button>
     </>
   );
 };
--- src/components/ui/buttonGroup/ButtonGroup.tsx
+++ src/components/ui/buttonGroup/ButtonGroup.tsx
@@ -1,8 +1,11 @@
+import classNames from 'classnames';
 import React, { ReactElement } from 'react';
 
 import './ButtonGroup.scss';
 
 type Props = {
+  activeIdx: number;
+  setActiveIdx: (idx: number) => void;
   children: ReactElement[];
 };
 
@@ -10,8 +13,11 @@ export const ButtonGroup: React.FC<Props> = (props) => {
   return (
     <div className="btn-group__container">
       {props.children.map((child, i) => {
+        const itemClasses = classNames('btn-group__item', {
+          'btn-group__item--active': props.activeIdx === i,
+        });
         return (
-          <div key={i} className="btn-group__item">
+          <div key={i} className={itemClasses} onClick={() => props.setActiveIdx(i)}>
             {child}
           </div>
         );
