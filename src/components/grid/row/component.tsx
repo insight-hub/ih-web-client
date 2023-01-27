10daaff1eee5f376f0a0459f9ae2e46421b851c5
saymurrmeow <r.savinovskii@gmail.com>
2023-01-27 13:27:22 +0300

    feat(ui): row component

--- src/components/grid/row/component.tsx
+++ src/components/grid/row/component.tsx
@@ -1,3 +1,23 @@
-import * as sc from 'styled-components';
+import React, { ReactNode } from 'react';
 
-export * from 'styled-components';
+import { AlignItems, JustifyContent } from 'src/components/layout';
+import { styled } from 'src/components/utils-styled-components';
+import { Layout } from 'src/components/layout';
+
+interface Props {
+  alignItems?: AlignItems;
+  jusifyContent?: JustifyContent;
+  children?: ReactNode;
+}
+
+const ScRow = styled(Layout)`
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wpap: wrap;
+  box-sizing: border-box;
+`;
+
+export const Row: React.FC<Props> = (props) => {
+  return <ScRow {...props} />;
+};
