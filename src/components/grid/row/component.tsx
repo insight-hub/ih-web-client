f30f5464e983e20f4a07213f043d998a2a435319
saymurrmeow <r.savinovskii@gmail.com>
2023-01-28 13:05:22 +0300

    fix: typo

--- src/components/grid/row/component.tsx
+++ src/components/grid/row/component.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 
-import { AlignItems, JustifyContent, Position } from 'src/components/layout';
+import { AlignItems, JustifyContent } from 'src/components/layout';
 import { styled } from 'src/components/utils-styled-components';
 import { Layout } from 'src/components/layout';
 
@@ -14,10 +14,10 @@ const ScRow = styled(Layout)`
   display: flex;
   flex: 0 1 auto;
   flex-direction: row;
-  flex-wpap: wrap;
+  flex-wrap: wrap;
   box-sizing: border-box;
 `;
 
 export const Row: React.FC<Props> = (props) => {
-  return <ScRow position={Position.Relative} {...props} />;
+  return <ScRow {...props} />;
 };
