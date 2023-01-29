7ffe18ce799583d13e7144b66e5d2c9889e9a43f
saymurrmeow <r.savinovskii@gmail.com>
2023-01-29 06:44:16 +0300

    feat(ui): base test component

---
+++ src/components/text/component.tsx
@@ -0,0 +1,49 @@
+import { StandardLonghandProperties } from 'csstype';
+import { ReactNode } from 'react';
+
+import { styled } from '../utils-styled-components';
+import { getLayoutStyles, mapProps, PropClassNameMap, toCssProp } from '../layout';
+
+export enum textType {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  P = 'p',
+  Span = 'span',
+}
+
+interface NonCssProp {
+  type?: textType;
+  children: ReactNode;
+}
+
+interface CssProps {
+  weigth?: StandardLonghandProperties['fontWeight'];
+  size?: StandardLonghandProperties['fontSize'];
+  color?: StandardLonghandProperties['color'];
+}
+
+type Props = NonCssProp & CssProps;
+
+const textPropMap: PropClassNameMap<Required<CssProps>> = {
+  ...mapProps,
+  weigth: toCssProp('font-weight'),
+  size: toCssProp('font-size'),
+  color: toCssProp('color'),
+};
+
+export const Text = styled.p
+  .attrs((props: Props) => ({
+    as: props.type && textType.P,
+  }))
+  .withConfig({
+    shouldForwardProp: (prop, fn) => {
+      if (prop in textPropMap) {
+        return false;
+      }
+
+      return fn(prop);
+    },
+  })<Props>((props) => getLayoutStyles(props, textPropMap));
+
+Text.displayName = 'Text';
