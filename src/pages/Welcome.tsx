02535ff82f6c95c7184f0b968fe77a3df59592f9
saymurrmeow <r.savinovskii@gmail.com>
2023-02-09 19:00:49 +0300

    feat(pages): base welcome page layout

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import { AlignItems, Button, Color, Column, JustifyContent, Layout, Row, Text } from 'src/components';
+import React, { SyntheticEvent, useRef } from 'react';
+import {
+  AlignItems,
+  BorderRadius,
+  Button,
+  Color,
+  Column,
+  Display,
+  FlexDirection,
+  Input,
+  JustifyContent,
+  Label,
+  Layout,
+  Row,
+  Text,
+  Form,
+} from 'src/components';
 import { textType } from 'src/components/text';
 
 export const Welcome = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleCreateAccount = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <Row height="3rem" alignItems={AlignItems.Center} justifyContent={JustifyContent.Between}>
@@ -14,19 +35,46 @@ export const Welcome = () => {
           <Button variant="outline">Sing up</Button>
         </Layout>
       </Row>
-      <Row>
-        <Column cols={8}>
-          <Row padding={{ top: 5 }}>
-            <Text type={textType.H1} weigth="bold" size="2.5rem" color="white">
-              Discover and Connect with Like-Minded People.
+      <Row padding={{ top: 5 }}>
+        <Column cols={6}>
+          <Text type={textType.H1} weigth="bold" size="2.5rem" color="white">
+            Discover and Connect with Like-Minded People.
+          </Text>
+          <Layout padding={{ top: 0.5 }}>
+            <Text size={'1.3rem'} color={Color.TextTransparent}>
+              Community dedicated to sharing knowledge, building connections, and&#160;growing
+              together. Join now to connect with experts and explore new ideas.
             </Text>
-          </Row>
-          <Row padding={{ top: 0.5 }}>
-            <Text size={'1.5rem'} color={Color.TextTransparent}>
-              Community dedicated to sharing knowledge, building connections, and growing together.
-              Join now to connect with experts and explore new ideas.
+          </Layout>
+        </Column>
+        <Column offset={1} cols={5}>
+          <Form
+            display={Display.Flex}
+            flexDirection={FlexDirection.Column}
+            padding={{ x: 2, y: 2 }}
+            borderRadius={BorderRadius.Medium}
+            backgroundColor={Color.White}
+            ref={formRef}
+          >
+            <Label id="username" label="Username" />
+            <Input name="username" />
+            <Label id="email" label="Email" />
+            <Input name="email" />
+            <Label id="password" label="Password" />
+            <Input name="password" />
+            <Text size="0.8rem" color="rgba(0, 0, 0, .7)">
+              Make sure it's at least 15 characters OR at least 8 characters including a number and
+              a lowercase letter.
             </Text>
-          </Row>
+            <Button onClick={handleCreateAccount} size="lg" variant="primary">
+              Create account
+            </Button>
+
+            <Text size="0.8rem" color="rgba(0, 0, 0, .7)">
+              By clicking «Create account», you agree to our <a href="/">Terms of Service</a> and{' '}
+              <a href="/">Privacy Statement</a>. We’ll occasionally send you account related emails.
+            </Text>
+          </Form>
         </Column>
       </Row>
     </>
