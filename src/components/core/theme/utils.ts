4240f5461fcab84eeef2c6b71e54b1e3580ce508
saymurrmeow <r.savinovskii@gmail.com>
2023-02-10 14:23:36 +0300

    wip(theme): utils

--- src/components/core/theme/utils.ts
+++ src/components/core/theme/utils.ts
@@ -1,3 +1,9 @@
+import Theme, { ThemeContextValue } from './theme';
+
 export function toRem(val: number) {
   return val / 16 + 'rem';
+}
+
+export function injectThemeValue(val: keyof ThemeContextValue) {
+  return ({ theme }: { theme: typeof Theme }) => theme[val];
 }
