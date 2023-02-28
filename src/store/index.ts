dea8ce9d1824d9a2a6a9d4ce2db1a34254e94e7f
saymurrmeow <r.savinovskii@gmail.com>
2023-02-28 22:58:09 +0300

    feat(store): redux-logger, thunk middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,37 @@
-import { configureStore, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import profileReducer from './profile/reducer';
 
-const rootReducer = createReducer({}, (builder) => { });
+export const createAccount = createAction<{}, 'createAccount'>('createAccount');
+
+const rootReducer = createReducer(
+  {
+    username: '',
+    email: '',
+    password: '',
+  },
+  (builder) => {
+    builder.addCase({ type: 'test' }, (state, action) => {
+      console.log(action);
+      console.log('test');
+      state.username = 'test';
+    });
+  },
+);
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'prod') {
+  middleware.push(createLogger());
+}
 
 const store = configureStore({
   reducer: {
     rootReducer,
     profileReducer,
   },
+  middleware,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
